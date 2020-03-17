faa9d43aff5cd1b3945613790fa8292d8f95b747
markchang <h03514@gmail.com>
2020-03-17 22:21:12 +0800

    初版完成 尚未加入查詢功能 昨日錯誤原因(X,Y軸搞反)

--- js/common.js
+++ js/common.js
@@ -48,62 +48,32 @@
         });
     }
 
+
     function masksInfo(da) {
         let data = da.json;
-        let width = $(window).width();
-        let height = $(window).height();
-        let myMap = L.map('mapid').setView([25.0722631, 121.5816963], 16);
-        L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
-            maxZoom: 16,
-            attribution: 'Map data: © <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: © <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
-        }).addTo(myMap);
 
-        // L.circleMarker([25.0722631, 121.5816963], {
-        //     radius: 10
-        // }).addTo(myMap);
+        let maskMap = L.map('mapid').setView([25.0722631, 121.5816963], 16);
 
-
-        const greenIcon = L.icon({
-            iconUrl: '../images/marker-icon.png',
-            iconSize: [50, 50], // size of the icon
-            // popupAnchor: [0, -10] // point from which the popup should open relative to the iconAnchor
-        });
-        // L.marker(data.features[i].geometry.coordinates, { icon: greenIcon }).addTo(myMap);
-
-
+        L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+            attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        }).addTo(maskMap);
 
         let locateData = [];
-
-
         for (let i in data.features) {
-
-            if (i > 20) {
-                continue;
-            }
             let locateDataInfo = {};
-            // locateDataInfo.name = data.features[i].properties.name;
-            locateDataInfo.local = data.features[i].geometry.coordinates;
+            locateDataInfo.name = data.features[i].properties.name;
+            locateDataInfo.mask_adult = data.features[i].properties.mask_adult;
+            locateDataInfo.mask_child = data.features[i].properties.mask_child;
+            locateDataInfo.localY = data.features[i].geometry.coordinates[0];
+            locateDataInfo.localX = data.features[i].geometry.coordinates[1];
             locateData.push(locateDataInfo);
-
         }
-        cslog(locateData);
-        // L.marker(data.features[i].geometry.coordinates, { icon: greenIcon }).addTo(myMap);
-        // console.log(locateData);
-        // cslog(greenIcon);
+
+        const markers = new L.MarkerClusterGroup();
         locateData.forEach(item => {
-            L.marker(item.local, {
-                    // title: item.name,
-                    icon: greenIcon
-                })
-                .addTo(myMap)
-                // .bindPopup(item.name);
+            markers.addLayer(L.marker([item.localX, item.localY]).bindPopup(`<ul><li>${item.name}</li><li>成人口罩: ${item.mask_adult}</li><li>兒童口罩: ${item.mask_child}</li></ul>`));
+
         });
+        maskMap.addLayer(markers);
 
-        // L.marker([locateData[0].locate[0], locateData[0].locate[1]], { icon: greenIcon }).addTo(myMap);
-        // L.marker(locateData[i].locate, { icon: greenIcon }).addTo(myMap);
-
-        // for (let i in locateData) {
-        //     // L.marker(locateData[i].locate, { icon: greenIcon }).addTo(myMap);
-        //     cslog(locateData[i].locate);
-        // }
     }
